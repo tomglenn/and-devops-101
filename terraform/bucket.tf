16d3120cfff6d445655b5a39dddf1519161ef06d
Romans Malinovskis <me@nearly.guru>
2020-02-04 12:53:21 +0000

    add terraform for day2 static site publishing

---
+++ terraform/bucket.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "eu-west-2"
+
+}
+
+resource "aws_s3_bucket" "andtest" {
+  bucket = "andacademy-test3"
+  acl = "public-read"
+  tags = {
+    Owner        = "romans"
+    Environment = "and academy"
+  }
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::andacademy-test3/*",
+      "Principal": {
+        "AWS": [
+          "*"
+        ]
+      }
+    }
+  ]
+}
+EOF
+}
+
+// Pollicy allows to put files into the bucket
+resource "aws_iam_policy" "andtest-deploy" {
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+         "Effect":"Allow",
+         "Action":[
+            "s3:PutObject",
+            "s3:PutObjectAcl",
+            "s3:GetObject",
+            "s3:GetObjectAcl",
+            "s3:DeleteObject"
+         ],
+         "Resource":"${aws_s3_bucket.andtest.arn}/*"
+      }
+  ]
+}
+EOF
+
+}
+
+
+resource "aws_iam_user_policy_attachment" "user-policy" {
+  policy_arn = aws_iam_policy.andtest-deploy.arn
+  user = aws_iam_user.deploy-user.id
+}
+
+resource "aws_iam_user" "deploy-user" {
+  name = "devops-academy-deploy-user"
+}
+
+resource "aws_iam_access_key" "deploy-user-access" {
+  user = aws_iam_user.deploy-user.id
+}
+
+output "access_key" {
+  value = aws_iam_access_key.deploy-user-access.id
+}
+output "secret_access_key" {
+  value = aws_iam_access_key.deploy-user-access.secret
+}
