3c497ddbdfe9423906034bfb8b99f374cb785f80
Tom Glenn <tom.glenn@lloydsbanking.com>
2020-09-15 11:28:58 +0100

    adds catfact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://cat-fact.herokuapp.com/facts/random')
+      .then(res => {
+        console.log(res);
+      })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
